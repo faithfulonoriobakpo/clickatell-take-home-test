77df81abbdfe969b86a220934dbd6198ad25cbe3
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2025-03-09 19:20:42 +0100

    created websocket server

---
+++ real-time-activity-tracker-react/server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const PORT = 4000;
+const app = express();
+
+app.use(cors());
+
+// Create HTTP server
+const server = createServer(app);
+
+// Create Socket.IO server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+console.log(`WebSocket server listening on ws://localhost:${PORT}`);
+
+const activities = [
+  "logged in",
+  "posted a comment",
+  "updated profile",
+  "uploaded a file",
+  "liked a post",
+];
+
+const users = [
+  "Bolanle",
+  "Damilare",
+  "Faithful",
+  "Musa",
+  "Junaid",
+];
+
+const randomActivity = () => {
+  const randomActivity = activities[Math.floor(Math.random() * activities.length)];
+  const randomUser = users[Math.floor(Math.random() * users.length)];
+
+  return {
+    user: randomUser,
+    message: randomActivity,
+    timestamp: new Date().toLocaleTimeString()
+  }
+};
+
+function sendRandomActivity(){
+  setInterval(() => {
+    const activity = randomActivity();
+
+    // Broadcast to all clients
+    io.emit("activity", activity);
+  }, 5000);
+}
+
+// Socket.IO connection handling
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.emit("activity", randomActivity());
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+sendRandomActivity();
